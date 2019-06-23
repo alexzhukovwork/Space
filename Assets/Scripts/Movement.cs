d88c1828c9212304f46e1ffd6f5576309f841aa7
Alexey <alexzhukovwork@yandex.ru>
2019-06-23 11:50:32 +0300

    Fixed bugs with movement, added counter for explosions

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-	[SerializeField]
-	private float speed;
+	[SerializeField] private float speed;
+
+	[SerializeField] private float maxSpeed;
+
+	[SerializeField] private int maxCount = 0;
+
+	[SerializeField] int currentCount = 0;
 	
 	private Transform _transform;
 	private Rigidbody2D _rb;
@@ -19,50 +24,35 @@ public class Movement : MonoBehaviour
 		_rb = GetComponent<Rigidbody2D>();
 		_respawn = _transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		Vector3 dir = Vector3.zero;
-
-	/*	if (Input.GetKey(KeyCode.W))
-		{
-			dir += Vector3.up * speed;
-		}
-		
-		if (Input.GetKey(KeyCode.S))
-		{
-			dir += Vector3.down * speed;
-		}
-		
-		if (Input.GetKey(KeyCode.D))
-		{
-			dir += Vector3.right * speed;
-		}
-		
-		if (Input.GetKey(KeyCode.A))
-		{
-			dir += Vector3.left * speed;
-		}
-		*/
-	//	_rb.AddForce(dir * Time.deltaTime);
-		
-	/*	if (Input.touchCount > 0)
-		{
-	//		Debug.Log(Input.GetTouch(0).position);
-		}
-*/
-	}
 
 	private void OnCollisionEnter2D(Collision2D other)
 	{
 		if (other.gameObject.tag.Equals("Dead"))
+		{
 			_transform.position = _respawn;
+			currentCount = 0;
+		}
 	}
 
 	public void AddForceFromPoint(Vector3 p)
 	{
-		Debug.Log("Normal " + (_transform.position - p).normalized);
-		_rb.AddForceAtPosition((_transform.position - p).normalized * speed*  (1.0f / ((_transform.position - p).magnitude / 5)), p, ForceMode2D.Impulse);
+		if (maxCount != 0 && currentCount >= maxCount)
+			return;
+		
+		if (maxCount != 0)
+			currentCount++;
+		
+		p.z = 0;
+		Vector3 position = _transform.position - p;
+		float coeff = 1.0f / position.sqrMagnitude / 10f;
+		float currentSpeed = Mathf.Min(coeff * speed, maxSpeed);
+		
+	//	Debug.Log("Normal " + (_transform.position - p).normalized);
+	//	Debug.Log("Distance " + (_transform.position - p).magnitude);
+	//	Debug.Log("Distance SQRT " + (_transform.position - p).sqrMagnitude);
+		
+		Debug.Log("Coeff " + coeff);
+
+		_rb.AddForceAtPosition(currentSpeed * position.normalized, p, ForceMode2D.Impulse);
 	}
 }
