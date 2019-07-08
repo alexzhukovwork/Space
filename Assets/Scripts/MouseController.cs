2fca64a4fd8cab6b1f9fa1f15961d95d014e5c0d
Alexey <alexzhukovwork@yandex.ru>
2019-07-08 21:11:53 +0300

    Added touch controller

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using WebClientSDK.Scripts.StateMachine;
 
-public class MouseController : MonoBehaviour {
+public class MouseController : MonoBehaviour
+{
+	[SerializeField] private bool isAndroid = false;
+	
 	private Movement _movement;
 	// Use this for initialization
 	void Start ()
@@ -16,10 +19,19 @@ public class MouseController : MonoBehaviour {
 	void Update () {
 	//	if (AppStateManager.Instance.CurrentlyApplicationState == States.StateApp.Menu)
 	//		return;
-		
-		if (Input.GetMouseButtonDown(0))
+	
+		if (!isAndroid && Input.GetMouseButtonDown(0))
 		{
 			Vector3 p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			Debug.Log(p);
+			
+			_movement.AddForceFromPoint(p);
+			
+		} 
+		else if (isAndroid && Input.touchCount > 0)
+		{
+			Vector3 p = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
 
 			Debug.Log(p);
 			
