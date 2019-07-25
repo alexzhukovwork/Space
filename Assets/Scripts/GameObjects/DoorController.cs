57144d38881e50e53100d5e3b25bd9ca0b334382
Alexey <alexzhukovwork@yandex.ru>
2019-07-25 22:25:06 +0300

    Added simple door

---
+++ Assets/Scripts/GameObjects/DoorController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _Left;
+    [SerializeField] private GameObject _Right;
+
+    [SerializeField] private float _TimeClose;
+    [SerializeField] private float _TimeOpen;
+
+    private Vector3 _open = new Vector3(0, 0, 90);
+
+    private void Awake()
+    {
+        Open();
+    }
+
+    private void Open()
+    {
+        _Left.transform.DORotate(_open * -1, 1, RotateMode.Fast);
+        _Right.transform.DORotate(_open, 1, RotateMode.Fast);
+        
+        Invoke("Close", _TimeOpen + 1);
+        
+
+    }
+
+    private void Close()
+    {
+        _Left.transform.DORotate(Vector3.zero, 1, RotateMode.Fast);
+        _Right.transform.DORotate(Vector3.zero, 1, RotateMode.Fast);
+        
+        Invoke("Open", _TimeOpen + 1);
+
+    }
+    
+}
--- Assets/Scripts/Logic/BigExplosion.cs
+++ Assets/Scripts/Logic/BigExplosion.cs
@@ -8,14 +8,36 @@ public class BigExplosion : MonoBehaviour
 
 	[SerializeField] private float _DeadDistance = 1f;
 
+	private SpriteRenderer _spriteRenderer;
+
 	private void Awake()
 	{
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+		_spriteRenderer.enabled = false;
 		Messenger<Vector3>.AddListener(InputEvents.PressTouch.ToString(), Explode);
 	}
 
 	private void Explode(Vector3 p)
 	{
-		Messenger<Vector3, float>.Broadcast(GameEvents.BigExplosion.ToString(), p, _DeadDistance);
+		
+		
+		_spriteRenderer.enabled = true;
+		transform.position = p;
+		
+		
+		
+		
+		
+		
+		
+		
+		Invoke("Disable", 1);
+	}
+
+	private void Disable()
+	{
+		_spriteRenderer.enabled = false;
+		Messenger<Vector3, float>.Broadcast(GameEvents.BigExplosion.ToString(), transform.position, _DeadDistance);
 	}
 	
 }
